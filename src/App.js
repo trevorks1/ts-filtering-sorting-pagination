ceba0b87570cd32cebe985c0ee57d798c16c552a
TrevorKS1 <trevorksansalone@gmail.com>
2021-06-15 08:42:28 -0500

    setting up core framework in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {loadData} from "./store";
+ 
+class App extends Component {
+  
+   componentDidMount() {
+       //loadData returns an object used by dispatch as the action
+       //{count: 20} is our payload
+       this.props.dispatch(loadData({count: 20}));
+   }
+ 
+   render() {
+       return (
+           <div className="App">
+               <section className="hero">
+                   <div className="hero-body">
+                       <div className="container">
+                           <h1 className="title">
+                               Pagination, Filtering and Sorting with React
+                           </h1>
+                           <h2 className="subtitle">
+                               A detailed guide
+                           </h2>
+                       </div>
+                   </div>
+               </section>
+               <section className='section'>
+                   <div className='container'>
+                       <div>
+                           <div className="field is-grouped" style={{alignItems: "center"}}>
+                               <div className="control">
+                                   <div className="select">
+                                       <select>
+                                           <option value="" disabled selected>Sort by</option>
+                                           <option>Price - Lowest to Highest</option>
+                                           <option>Price - Highest to Lowest</option>
+                                           <option>Alphabet - A-Z</option>
+                                           <option>Alphabet - Z-A</option>
+                                       </select>
+                                   </div>
+                               </div>
+ 
+                               <div className='control' style={{minWidth: "300px"}}>
+                                   <input style={{width: "100%"}} placeholder='Filter by' type='text'/>
+                               </div>
+                           </div>
+                       </div>
+                       <div className='tile is-ancestor' style={{flexWrap: "wrap"}}>
+                           {
+                               this.props.state.products && this.props.state.products.map(product => (
+                                   <div className='tile is-parent is-3'>
+                                       <div className='tile is-child box'>
+                                           <p>
+                                               <b>Name: </b>
+                                               {product.name}
+                                           </p>
+                                           <p>
+                                               <b>Designer: </b>
+                                               {product.designer}
+                                           </p>
+                                           <p>
+                                               <b>Type: </b>
+                                               {product.type}
+                                           </p>
+                                           <p>
+                                               <b>Price: </b>
+                                               {product.price}
+                                           </p>
+                                       </div>
+                                   </div>
+                               ))
+                           }
+                       </div>
+                   </div>
+               </section>
+           </div>
+ 
+       );
+   }
 }
-
-export default App;
+ 
+function mapStateToProps(state) {
+   return {state};
+}
+ 
+export default connect(mapStateToProps)(App);
