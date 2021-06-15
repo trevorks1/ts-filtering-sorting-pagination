007a6268d15049d04cd5b4d3e9acc8ff7be2a930
TrevorKS1 <trevorksansalone@gmail.com>
2021-06-15 09:15:54 -0500

    fixing file pathing

--- src/App.js
+++ src/App.js
@@ -1,87 +1,156 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {loadData} from "./store";
- 
+import {filterByValue, loadData, loadExactPage, loadNewPage, sortByAlphabet, sortByPrice} from "./store";
+
 class App extends Component {
-  
-   componentDidMount() {
-       //loadData returns an object used by dispatch as the action
-       //{count: 20} is our payload
-       this.props.dispatch(loadData({count: 20}));
-   }
- 
-   render() {
-       return (
-           <div className="App">
-               <section className="hero">
-                   <div className="hero-body">
-                       <div className="container">
-                           <h1 className="title">
-                               Pagination, Filtering and Sorting with React
-                           </h1>
-                           <h2 className="subtitle">
-                               A detailed guide
-                           </h2>
-                       </div>
-                   </div>
-               </section>
-               <section className='section'>
-                   <div className='container'>
-                       <div>
-                           <div className="field is-grouped" style={{alignItems: "center"}}>
-                               <div className="control">
-                                   <div className="select">
-                                       <select>
-                                           <option value="" disabled selected>Sort by</option>
-                                           <option>Price - Lowest to Highest</option>
-                                           <option>Price - Highest to Lowest</option>
-                                           <option>Alphabet - A-Z</option>
-                                           <option>Alphabet - Z-A</option>
-                                       </select>
-                                   </div>
-                               </div>
- 
-                               <div className='control' style={{minWidth: "300px"}}>
-                                   <input style={{width: "100%"}} placeholder='Filter by' type='text'/>
-                               </div>
-                           </div>
-                       </div>
-                       <div className='tile is-ancestor' style={{flexWrap: "wrap"}}>
-                           {
-                               this.props.state.products && this.props.state.products.map(product => (
-                                   <div className='tile is-parent is-3'>
-                                       <div className='tile is-child box'>
-                                           <p>
-                                               <b>Name: </b>
-                                               {product.name}
-                                           </p>
-                                           <p>
-                                               <b>Designer: </b>
-                                               {product.designer}
-                                           </p>
-                                           <p>
-                                               <b>Type: </b>
-                                               {product.type}
-                                           </p>
-                                           <p>
-                                               <b>Price: </b>
-                                               {product.price}
-                                           </p>
-                                       </div>
-                                   </div>
-                               ))
-                           }
-                       </div>
-                   </div>
-               </section>
-           </div>
- 
-       );
-   }
+
+    componentDidMount() {
+        const params = new URLSearchParams(window.location.search);
+        const pageQueryParam = params.get('page');
+        if (!pageQueryParam) {
+            window.history.pushState({page: 1}, "title 1", "?page=1");
+        } else {
+
+        }
+        this.props.dispatch(loadData({count: 40}));
+    }
+
+    filterByInput(e){
+        let input = e.target.value;
+        this.props.dispatch(filterByValue({value: input}))
+    }
+
+    nextPage() {
+        this.props.dispatch(loadNewPage({page: 1}))
+    }
+
+    previousPage() {
+        this.props.dispatch(loadNewPage({page: -1}));
+    }
+
+    goToPage(page) {
+        this.props.dispatch(loadExactPage({page}))
+    }
+
+    sortByInput(e){
+        let value = e.target.value;
+        let direction = value.endsWith('asc') ? "asc" : "desc";
+
+        if (value.startsWith('price')){
+            this.props.dispatch(sortByPrice({direction}))
+        }else {
+            this.props.dispatch(sortByAlphabet({direction}));
+        }
+    }
+
+    render() {
+        let products = this.props.state.filteredProducts;
+        return (
+            <div className="App">
+                <section className="hero">
+                    <div className="hero-body">
+                        <div className="container">
+                            <h1 className="title">
+                                Pagination, Filtering and Sorting with React
+                            </h1>
+                            <h2 className="subtitle">
+                                A detailed guide
+                            </h2>
+                        </div>
+                    </div>
+                </section>
+                <section className='section'>
+                    <div className='container'>
+                        <nav className="pagination" role="navigation" aria-label="pagination">
+                            <button className="button pagination-previous" onClick={() => {
+                                this.previousPage()
+                            }}>Previous
+                            </button>
+                            <button className="button pagination-next" onClick={() => {
+                                this.nextPage()
+                            }}>Next page
+                            </button>
+                            <ul className="pagination-list">
+                                {
+                                    [...Array(this.props.state.filteredPages)].map((value, index) => (
+                                        <button
+                                            className={`button pagination-link ${this.props.state.currentPage === index + 1 ? "is-current" : ""}`}
+                                            aria-label="Page 1"
+                                            onClick={() => this.goToPage(index + 1)}
+                                            aria-current="page">
+                                            {index + 1}
+                                        </button>
+                                    ))
+                                }
+                            </ul>
+                        </nav>
+
+                    </div>
+                </section>
+                <section className='section'>
+                    <div className='container'>
+                        <div>
+                            <div className="field is-grouped" style={{alignItems: "center"}}>
+                                <div className="control">
+                                    <div className="select">
+                                        <select onChange={e => {
+                                            this.sortByInput(e)
+                                        }}>
+                                            <option value="" disabled selected>Sort by</option>
+
+                                            <option value='alphabet_asc'>Name - A-Z</option>
+                                            <option value='alphabet_desc'>Name - Z-A</option>
+
+                                            <option value='price_asc'>Price - Lowest to Highest</option>
+                                            <option value='price_desc'>Price - Highest to Lowest</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className='control' style={{minWidth: "300px"}}>
+                                    <input onChange={e => {
+                                        this.filterByInput(e);
+                                    }} style={{width: "100%"}} placeholder='Filter by' type='text'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='tile is-ancestor' style={{flexWrap: "wrap"}}>
+                            {
+                                products && products.length && products.map(product => (
+                                    <div className='tile is-parent is-3'>
+                                        <div className='tile is-child box'>
+                                            <p>
+                                                <b>Name: </b>
+                                                {product.name}
+                                            </p>
+                                            <p>
+                                                <b>Designer: </b>
+                                                {product.designer}
+                                            </p>
+                                            <p>
+                                                <b>Type: </b>
+                                                {product.type}
+                                            </p>
+                                            <p>
+                                                <b>Price: </b>
+                                                ${product.price}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        );
+    }
 }
- 
+
 function mapStateToProps(state) {
-   return {state};
+    return {state};
 }
- 
+
 export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
-import './index.css'; 
-import App from './App'; 
-import {Provider} from "react-redux"; 
-import {createStore} from "redux"; 
-import reducers from "./store"; 
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from "./store";
 
-// storing createStore in a variable declaration for our reducers
-const store = createStore(reducers); 
+const store = createStore(reducers);
+
 ReactDOM.render(<Provider store={store}>
         <App/>
     </Provider>,
     document.getElementById('root')
-);     
-  
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
-
- 
+);
